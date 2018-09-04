af9138682749464f27d8a39dffe7328ed6501988
alu0100826999 <alu0100826999@ull.edu.es>
2018-09-04 22:37:37 +0100

    Reorganizando carpetas y actualizando Memoria TFG

--- code/blockly-master/principal-page/js/modal.js
+++
@@ -1,7 +0,0 @@
-$(document).on('click', '#Crear', function() {
-  $('#ModalCrear').modal('show');
-});
-
-$(document).on('click', '#agregar_nombres', function() {
-  $('#ModalAgregarNombre').modal('show');
-});
--- code/blockly-master/xml-firefox.js
+++
@@ -1,7 +0,0 @@
-function documentLoaded (e) {
-  var sXML = new XMLSerializer().serializeToString(e.target); // Gives querydata.xml contents as string
-}
-
-var xmlDoc = document.implementation.createDocument("", "", null);
-xmlDoc.addEventListener("load", documentLoaded, false);
-xmlDoc.load('config_robot.xml');
